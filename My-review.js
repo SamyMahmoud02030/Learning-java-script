6269e947c34162fe8a3390b922f5271fe13744fa
samy-Mahmoud <samymahmoud20030@gmail.com>
2024-05-13 23:42:17 +0300

    new join one secondary holiday

--- My-review.js
+++ My-review.js
@@ -913,3 +913,74 @@ console.log(userC);
 console.log(userC.age);
 console.log(userC.cantry);
 console.log(userC.sayHello())
+
+
+
+/*
+  Function this Keyword
+  Search
+  - Strict Mode
+*/
+
+console.log(this);
+console.log(this === window);
+
+myVar = 100;
+
+console.log(window.myVar);
+console.log(this.myVar);
+
+function sayHello() {
+  console.log(this);
+  return this;
+}
+sayHello();
+
+console.log(sayHello() === window);
+
+let userD = {
+  age: 38,
+  ageInDays: function () {
+    console.log(this);
+    return this.age * 365;
+  },
+};
+
+console.log(userD.age);
+console.log(userD.ageInDays());
+
+
+/*
+  Object
+  - Create Object With Assign Method
+*/
+
+let obj1 = {
+  prop1: 1,
+  meth1: function () {
+    return this.prop1;
+  },
+};
+
+let obj2 = {
+  prop2: 2,
+  meth2: function () {
+    return this.prop2;
+  },
+};
+
+let targetObject = {
+  prop1: 100,
+  prop3: 3,
+};
+
+let finalObject = Object.assign(targetObject, obj1, obj2);
+
+finalObject.prop1 = 200;
+finalObject.prop4 = 4;
+
+console.log(finalObject);
+
+let newObject = Object.assign({}, obj1, { prop5: 5, prop6: 6 });
+
+console.log(newObject);
